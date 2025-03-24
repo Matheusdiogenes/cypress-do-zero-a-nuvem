065b8eae7986dc1b1962e012c7a76c86a138ca85
Matheus Diogenes <matheusdiogenes98@gmail.com>
2025-03-24 17:10:04 -0300

    Session 2 - exercise from class 9 completed

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,6 +1,34 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('verifica o título da aplicação', () => {
+  beforeEach(() => {
     cy.visit('src/index.html')
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT'); 
+  })
+
+  it('verifica o título da aplicação', () => {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
+  })
+
+  it('Mensagem: Valide os campos obrigatórios!', () => {
+    cy.get('#firstName').type('Jorge')
+    cy.get('#lastName').type('Dela Lafon')
+    cy.get('#email').type('teste')
+
+    cy.get('.button').click()
+    cy.get('.error').should('be.visible').invoke('text')
+      .then((texto) => {
+        expect(texto.trim()).to.include('Valide os campos obrigatórios!')
+      })
+  })
+
+  it('Sucess!', () => {
+    cy.get('#firstName').type('Chico')
+    cy.get('#lastName').type('Bento')
+    cy.get('#email').type('matheus@gmail.com')
+    cy.get('#open-text-area').type('esse é o teste')
+
+    cy.get('.button').click()
+    cy.get('.success').should('be.visible').invoke('text')
+      .then((texto) => {
+        expect(texto.trim()).to.include('Mensagem enviada com sucesso.')
+      })
   })
 })
